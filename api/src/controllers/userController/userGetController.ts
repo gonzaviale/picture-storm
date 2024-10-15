fd1faa1f99f3c0407688df5242da91f5a0d036aa
gonzaviale <gonzaloleonelviale@gmail.com>
2024-10-15 16:29:21 -0300

    paginado

--- api/src/controllers/userController/userGetController.ts
+++ api/src/controllers/userController/userGetController.ts
@@ -3,33 +3,19 @@ import { verifyToken } from '../../utils/verifyToken';
 import getSavedPicturesForUserService from '../../services/getSavedPicturesForUserService';
 import Picture from '../../models/Picture';
 
-export const getPicturesByUserSaved = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+export const getPicturesByUserSaved = async (req: Request, res: Response) => {
+    try {
+      const authHeader = req.headers['authorization'];
+      const token = authHeader && authHeader.split(' ')[1];
+      const { username } = verifyToken(token!);
 
-    const { username } = verifyToken(token!);
+      const results = await getSavedPicturesForUserService(username as string);
 
-    const page = parseInt(req.query.page as string) || 1;
-    const limit = 4;
-    const offset = (page - 1) * limit;
-
-    const { savedPictures, totalCount } = await getSavedPicturesForUserService(username, offset, limit);
-
-    const totalPages = Math.ceil(totalCount / limit);
-
-    res.status(200).json({
-      pictures: savedPictures,
-      totalCount,
-      totalPages,
-      currentPage: page,
-      hasNextPage: page < totalPages,
-      hasPreviousPage: page > 1,
-    });
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching saved pictures'});
-  }
-};
+      res.status(200).json(results);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching saved pictures' });
+    }
+  };
 
   export const getPicturesByUser = async (req: Request, res: Response) => {
     try {
